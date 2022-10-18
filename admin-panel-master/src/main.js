53baf03d6d19eea2046b4f68f839d28a0c72f117
naeef <mochammadnayf@gmail.com>
2022-10-18 15:46:02 +0700

    deleted:    admin-panel-master/node_modules/.bin/husky

--- admin-panel-master/src/main.js
+++ admin-panel-master/src/main.js
@@ -5,11 +5,15 @@ import router from '@/router'
 import VueCookies from 'vue3-cookies'
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
-
+import {VueCsvImportPlugin} from "vue-csv-import";
+import JsonExcel from "vue-json-excel3";
 const app = createApp(App)
 app.use(VueCookies, {
     expireTimes: "7d",
 });
 app.use(VueSweetalert2);
-app.use(router)
+app.use(VueCsvImportPlugin);
+app.use(router);
+
+app.component("downloadExcel", JsonExcel);
 app.mount('#app')
