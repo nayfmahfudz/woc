8320c4e9554f7e50f8a5ab84936dc4bf072cc539
naeef <mochammadnayf@gmail.com>
2022-10-21 15:16:11 +0700

    new file:   beckendADMIN/models/DataPosModels.js
    Changes not staged for commit:
    	modified:   admin-panel-master/node_modules/.package-lock.json
    	modified:   admin-panel-master/node_modules/.vite/_metadata.json
    	modified:   admin-panel-master/node_modules/.vite/moment.js
    	modified:   admin-panel-master/node_modules/.vite/moment.js.map
    	modified:   admin-panel-master/node_modules/cliui/CHANGELOG.md
    	modified:   admin-panel-master/node_modules/cliui/package.json
    	modified:   admin-panel-master/node_modules/string-width/package.json
    	modified:   admin-panel-master/node_modules/strip-ansi/package.json
    	deleted:    admin-panel-master/node_modules/yargs/CHANGELOG.md
    	modified:   admin-panel-master/node_modules/yargs/README.md
    	modified:   admin-panel-master/node_modules/yargs/build/index.cjs
    	modified:   admin-panel-master/node_modules/yargs/build/lib/command.js
    	modified:   admin-panel-master/node_modules/yargs/build/lib/completion-templates.js
    	modified:   admin-panel-master/node_modules/yargs/build/lib/completion.js
    	modified:   admin-panel-master/node_modules/yargs/build/lib/middleware.js
    	modified:   admin-panel-master/node_modules/yargs/build/lib/usage.js
    	modified:   admin-panel-master/node_modules/yargs/build/lib/validation.js
    	modified:   admin-panel-master/node_modules/yargs/build/lib/yargs-factory.js
    	modified:   admin-panel-master/node_modules/yargs/build/lib/yerror.js
    	modified:   admin-panel-master/node_modules/yargs/index.cjs
    	modified:   admin-panel-master/node_modules/yargs/lib/platform-shims/browser.mjs
    	modified:   admin-panel-master/node_modules/yargs/lib/platform-shims/esm.mjs
    	modified:   admin-panel-master/node_modules/yargs/locales/ko.json
    	modified:   admin-panel-master/node_modules/yargs/locales/ru.json
    	modified:   admin-panel-master/node_modules/yargs/package.json
    	modified:   admin-panel-master/package-lock.json
    	modified:   admin-panel-master/package.json
    	modified:   admin-panel-master/src/components/layouts/Sidebar.vue
    	modified:   admin-panel-master/src/pages/Guest.vue
    	deleted:    admin-panel-master/src/pages/developments/tables/Index.vue
    	deleted:    admin-panel-master/src/pages/developments/tables/tablesIzin.vue
    	deleted:    admin-panel-master/src/pages/developments/tables/tablesStatus.vue
    	modified:   admin-panel-master/src/router/routes.js
    	modified:   beckendADMIN/config/config.js
    	deleted:    beckendADMIN/controller/izin.js
    	deleted:    beckendADMIN/models/izinModels.js
    	modified:   beckendADMIN/node_modules/.bin/acorn
    	modified:   beckendADMIN/node_modules/.bin/is-ci
    	modified:   beckendADMIN/node_modules/.bin/jade
    	modified:   beckendADMIN/node_modules/.bin/mime
    	modified:   beckendADMIN/node_modules/.bin/mkdirp
    	modified:   beckendADMIN/node_modules/.bin/nodemon
    	modified:   beckendADMIN/node_modules/.bin/nodetouch
    	modified:   beckendADMIN/node_modules/.bin/nopt
    	modified:   beckendADMIN/node_modules/.bin/rc
    	modified:   beckendADMIN/node_modules/.bin/semver
    	modified:   beckendADMIN/node_modules/.bin/uglifyjs
    	modified:   beckendADMIN/node_modules/make-dir/node_modules/.bin/semver
    	modified:   beckendADMIN/node_modules/package-json/node_modules/.bin/semver
    	modified:   beckendADMIN/node_modules/semver-diff/node_modules/.bin/semver
    	modified:   beckendADMIN/node_modules/with/node_modules/.bin/acorn
    	modified:   beckendADMIN/routes/index.js
    	modified:   package-lock.json
    
    Untracked files:
    	admin-panel-master/src/pages/developments/tables/dataTablePos.vue
    	admin-panel-master/src/pages/developments/tables/inputDatapos.vue
    	admin-panel-master/src/pages/developments/tables/inputPos.vue
    	beckendADMIN/controller/DataPos.js

---
+++ beckendADMIN/models/DataPosModels.js
@@ -0,0 +1,85 @@
+let db = require("../config/config.js");
+
+var date = new Date();
+// Get All Products
+exports.getDataPos = (result) => {
+    db.db.query("SELECT * FROM data_pos", (err, results) => {             
+        if(err) {
+            console.log(err);
+            result(err, null);
+        } else {
+            result(null, results);
+        }
+    });   
+}
+ 
+// Get Single Product
+exports.getDataPosById = (id, result) => {
+    db.db.query("SELECT * FROM data_pos WHERE id = ?", [id], (err, results) => {             
+        if(err) {
+            console.log(err);
+            result(err, null);
+        } else {
+            result(null, results[0]);
+        }
+    });   
+}
+
+// Insert Product to Laporan
+exports.insertLaporanDataPos = async (data, result) => {
+    error=null;
+    respon="";
+    await data.forEach(element => {
+    db.db.query("INSERT INTO laporan SET ?", element, (err, results) => {             
+        if(err) {
+            console.log(err);
+           eroor=err;
+        } else {
+            respon=results;
+        }
+    });
+});
+error!==null?result(error, null):result(null, respon);  
+}
+// Insert Product to Database
+exports.insertDataPos = async (data, result) => {
+    error=null;
+    respon="";
+    await data.forEach(element => {
+        console.log(element);
+     
+    db.db.query("INSERT INTO data_pos SET ?", element, (err, results) => {             
+        if(err) {
+            console.log(err);
+           eroor=err;
+        } else {
+            respon=results;
+        }
+    });
+});
+error!==null?result(error, null):result(null, respon);  
+}
+ 
+// Update Product to Database
+exports.updateDataPosById = (data, id, result) => {
+    db.db.query("UPDATE data_pos nama = ? , deskripsi = ? WHERE id = ?", [data.nama, data.deskripsi,id], (err, results) => {             
+        if(err) {
+            console.log(err);
+            result(err, null);
+        } else {
+            result(null, results);
+        }
+    });   
+}
+ 
+// Delete Product to Database
+exports.deleteDataPosById = (id, result) => {
+    db.db.query("DELETE FROM data_pos WHERE id = ?", [id], (err, results) => {             
+        if(err) {
+            console.log(err);
+            result(err, null);
+        } else {
+            result(null, results);
+        }
+    });
+}   
